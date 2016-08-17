e1fb91b8f6aafa88babfeebe0b3248b5debd1c68
karol <karol@dell.karols.eu>
2016-08-17 14:04:01 +0200

    commit

--- src/main/java/eu/karols/springtutorial/MainSpring.java
+++ src/main/java/eu/karols/springtutorial/MainSpring.java
@@ -1,10 +1,17 @@
 package eu.karols.springtutorial;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import eu.karols.springtutorial.api.UsersRepository;
+
 public class MainSpring {
 
 	public static void main(String[] args) {
-		
+		ApplicationContext context = new ClassPathXmlApplicationContext("configuration.xml");
 
+		UsersRepository usersRepository = context.getBean("repositoryUsers", UsersRepository.class);
+		usersRepository.createUser("Mietek");
 	}
 
 }
